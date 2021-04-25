3097710fae8dda80fd4b4f25c0bc34de61b398c0
CLIMS <lourdrealmdlrll@gmail.com>
2021-04-26 00:41:46 +0800

    Add Migration: Create Patient Table

---
+++ database/migrations/2021_04_23_133115_create_patients_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->boolean('gender');
+            $table->string('address');
+            $table->dateTime('date_of_birth');
+            $table->string('contact_number');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
