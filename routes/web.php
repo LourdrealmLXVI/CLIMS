4c4911c03563151d929fa9f279a6fed4da0d9063
CLIMS <lourdrealmdlrll@gmail.com>
2021-04-26 00:45:41 +0800

    Add Routes: Dashboard, and Group Patients

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard','DashboardController@index')->name('dashboard');
+
+Route::group(['prefix' => 'patients'],function(){
+    Route::get('/','PatientController@patients')->name('patient.index');
+    Route::get('/edit','PatientController@edit')->name('patient.edit');
+    Route::get('/create','PatientController@create')->name('patient.create');
+
+    Route::post('/create','PatientController@store')->name('patient.name');
+
+    Route::post('/update','PatientController@update')->name('patient.update');
+    Route::post('/delete','PatientController@delete')->name('patient.delete');
+    
+});
